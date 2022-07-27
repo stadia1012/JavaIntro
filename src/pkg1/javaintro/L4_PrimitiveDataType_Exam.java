e5835356fc073feab401dc45643214bd0e396c9e
stadia <stadia1012@gmail.com>
2022-07-28 00:15:50 +0900

    기본형 타입 끝

--- src/pkg1/javaintro/L4_PrimitiveDataType_Exam.java
+++ src/pkg1/javaintro/L4_PrimitiveDataType_Exam.java
@@ -1,7 +1,7 @@
 package pkg1.javaintro;
 
 public class L4_PrimitiveDataType_Exam {
-/*           기본형 타입
+/*           기본형[원시형] 타입
 	논리형_boolean : 1byte
 	문자형_char : 2byte
 	정수형 : byte(1), short(2), char(2), int(4), long(8)
@@ -14,12 +14,9 @@ public class L4_PrimitiveDataType_Exam {
 		System.out.println(isFun);  // true
 		
 		char c = 'a';  // 'ff' 등 한자리 초과 입력 시 오류
-		
 		long bing = 199610121122l;  // Long형은 끝에 반드시 L/l 입력해야.
-		
 		float f = 3.15f;  // 반드시 끝에 F/f 입력
-		
-		double d = 31415926535.5;  // d 안붙여도 됨
+		double d = 31415926535.5;  // **d 안붙여도 됨
 	}
 
 }
