40fa0179bb41de3d2f07b659e9112d7a2629fc96
stadia <stadia1012@gmail.com>
2022-07-29 00:21:57 +0900

    기본형의 형변환 끝

--- src/pkg1/javaintro/L4_PrimitiveDataType_Exam.java
+++ src/pkg1/javaintro/L4_PrimitiveDataType_Exam.java
@@ -7,7 +7,6 @@ public class L4_PrimitiveDataType_Exam {
 	정수형 : byte(1), short(2), char(2), int(4), long(8)
 	실수형 : float(4), double(8)
 	
-	리터럴
 */
 	public static void main(String[] args) {
 		boolean isFun = true;  // true/false 이외 값 입력 시 오류
@@ -17,6 +16,8 @@ public class L4_PrimitiveDataType_Exam {
 		long bing = 199610121122l;  // Long형은 끝에 반드시 L/l 입력해야.
 		float f = 3.15f;  // 반드시 끝에 F/f 입력
 		double d = 31415926535.5;  // **d 안붙여도 됨
+
+//		long, float은 l, f를 붙여줘야 한다.
 	}
 
 }
---
+++ src/pkg1/javaintro/L5_TypeCasting.java
@@ -0,0 +1,30 @@
+package pkg1.javaintro;
+
+public class L5_TypeCasting {
+/*				기본형의 형변환
+	묵시적 형변환이 가능한 관계도 (좌항은 우항에 담길 수 있다.)
+	byte < short,char < int < long < float < double
+*/
+	public static void main(String[] args) {
+		int x = 50000;
+		long y = x;  // 묵시적 형변환 가능
+		
+		
+		long x2 = 5;
+//		int y2 = x2;  // 실제로 값 5가 int에 들어갈 수 있지만 컴파일러는 판단 못함 오류 발생
+		int y2 = (int) x2;  // 값을 담기 전 int로 강제 형변환 진행
+		System.out.println(y2);  // 5
+		
+		long x3 = 10;
+		float y3 = x3;  // long(8)이 float(4)보다 byte 크기가 더 큰데도 가능.
+		System.out.println(y3);  // 10.0
+		
+		/* 실수를 int에 강제로 담으면?*/
+		double doubleValue = 123.456;
+		int intValue = (int) doubleValue;
+		System.out.println(intValue);  // 123  -> 소수점 아래는 삭제.
+		
+		System.out.println(doubleValue);  // 123.456 -> 기존 변수엔 영향 없음
+	}
+
+}
