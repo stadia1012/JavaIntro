83aabec300634c227712f4e214d7c7bcf2820a24
stadia <stadia1012@gmail.com>
2022-08-21 21:36:20 +0900

    if 수정

--- src/pkg1/javaintro/L8_IfAndTernaryOperator.java
+++ src/pkg1/javaintro/L8_IfAndTernaryOperator.java
@@ -4,7 +4,7 @@ public class L8_IfAndTernaryOperator {
 //               <if문과 삼항연산자>
 	public static void main(String[] args) {
 		int x = 50;
-		int y = 60;  // git commit 방지 default : 60으로 하기
+		int y = 60;  // 내용 변경으로 인한 commit 방지 default : 60으로 하기
 		
 		if ( x > y ) {
 			System.out.println("x는 y보다 크다.");
@@ -15,7 +15,7 @@ public class L8_IfAndTernaryOperator {
 		}
 		
 //		#실습1 : if문, 3의 배수이면 출력하기
-		int i1 = 12;
+		int i1 = 12;  // default : 12
 		
 		if ( i1 % 3 == 0 ) {
 			System.out.printf("%d는 3의 배수가 맞습니다.\n", i1);
@@ -27,7 +27,7 @@ public class L8_IfAndTernaryOperator {
 //		if문으로 대체 가능해 일반적으로 미사용
 		int i2 = (5 < 4) ? 50 : 40;
 		System.out.println(i2);  // 40
-//		괄호가 true이면 50, 아닐 경우 40
+//		괄호가 true이면 50, false면 40
 		
 //		#실습2 : 3항연산자
 //		hour가 12보다 작으면 AM, 그렇지 않으면 PM를 ampm에 저장
