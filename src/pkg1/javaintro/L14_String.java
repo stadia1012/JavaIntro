e7d357f0cb4f3d9bcb6dec80780a5f7d61ac3baf
stadia <stadia1012@gmail.com>
2022-08-31 23:47:14 +0900

    String 끝

--- src/pkg1/javaintro/L14_String.java
+++ src/pkg1/javaintro/L14_String.java
@@ -24,23 +24,32 @@ public class L14_String {
 		String str3 = new String("Hello");
 		String str4 = new String("Hello");
 		
-//		str1, str2와 str3, str4의 차이 :
-//		1, 2는 같은 인스턴스를 가르킨다. 값이 같으면 같은 인스턴스를 가리키도록 되어있기 때문
-//		2, 3은 new 연산자를 사용해 인스턴스가 각각 생성되었다.
-//		new 연산자가 붙으면 무조건 heap 영역에 새로 인스턴스 생성한다.
+/*		str1, str2와 str3, str4의 차이 :
+		1, 2는 같은 인스턴스를 가르킨다.
+		인스턴스가 상수 영역에 생성되어 같은 값이면 같은 인스턴스를 가르키게 되어있기 때문.
 		
-//		# 확인 방법
-		if( str1 == str2 ) {			// true
-			System.out.println("true");
+		2, 3은 new 연산자를 사용해 인스턴스가 각각 생성되었다.
+		new 연산자가 붙으면 무조건 heap 영역에 새로 인스턴스 생성한다. */
+		
+//		# 참조 인스턴스 확인
+		if( str1 == str2 ) {			// 같은 인스턴스를 참조
+			System.out.println("같은 인스턴스를 참조");
 		} else {
-			System.out.println("false");
+			System.out.println("서로 다른 인스턴스를 참조");
 		}
 		
-		if( str3 == str4 ) {			// false
-			System.out.println("true");
+		if( str3 == str4 ) {			// 서로 다른 인스턴스를 참조
+			System.out.println("같은 인스턴스를 참조");
 		} else {
-			System.out.println("false");
+			System.out.println("서로 다른 인스턴스를 참조");
 		}
-//		str1과 str2에 들어간 주소값이 같기 때문에 true
+		
+//		# 번외
+		if ( str3.equals(str4) ) {		// str3와 str4는 같은 값을 가짐
+			System.out.println("str3와 str4는 같은 값을 가짐");
+		} else {
+			System.out.println("str3와 str4는 서로 다른 값을 가짐");
+		}
+//		동위연산자 ==는 String 인스턴스가 같은지 비교하지만 .equals()메서드를 이용하면 값을 비교할 수 있음
 	}
 }
