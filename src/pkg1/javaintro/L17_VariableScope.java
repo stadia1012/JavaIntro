6fab65474854e540048e8a90bb8fa4624313f67b
stadia1012 <stadia1012@gmail.com>
2022-09-13 18:26:46 +0900

    scope 진행중

--- src/pkg1/javaintro/L17_VariableScope.java
+++ src/pkg1/javaintro/L17_VariableScope.java
@@ -1,5 +1,13 @@
 package pkg1.javaintro;
 
 public class L17_VariableScope {
-
+	int globalScope = 10;
+	
+	public void scopeTest( int value ) {
+		int localScope = 20;
+		System.out.println( globalScope );
+		System.out.println( localScope );
+		System.out.println( value );
+	}
+	
 }
