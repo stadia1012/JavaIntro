9ddd065945b6ddcf072ef96b0d489cecf3812a28
stadia <stadia1012@gmail.com>
2022-10-06 01:23:31 +0900

    생성자

--- src/pkg1/javaintro/L19_Constructor.java
+++ src/pkg1/javaintro/L19_Constructor.java
@@ -2,7 +2,17 @@ package pkg1.javaintro;
 
 public class L19_Constructor {
 //			   < 생성자 >
-//		new 연산자 뒤엔 반드시 생성자가 와야한다.
+//		객체 생성 시 필드를 초기화해주는 역할. new 연산자 뒤엔 반드시 생성자가 와야한다.
+	
+	/*
+	 * public 클래스명 (매개변수 목록) {
+	 * 		....
+	 * }     -> 생성자 예시
+	 * 
+	 * 메서드와 다르게 리턴타입이 없음.
+	 * 생성자를 명시해주지 않으면 매개변수 없는 생성자가 컴파일 시 자동으로 생성됨.
+	 * 
+	 */
 	public static void main(String[] args) {
 		
 		 
---
+++ src/pkg2/car/CarVer2.java
@@ -0,0 +1,8 @@
+package pkg2.car;
+/*	 < Car 클래스 >
+ * ver1
+*/
+public class CarVer2 {
+	public String name;  // public 접근 제어자 명시해 다른 패키지에서도 접근 가능.
+	public int number;
+}
