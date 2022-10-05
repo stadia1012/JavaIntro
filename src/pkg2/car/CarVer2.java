6f9b0b13aaf68e28d6411a2b21f03106f5065bc3
stadia <stadia1012@gmail.com>
2022-10-06 01:23:40 +0900

    생성자

--- src/pkg2/car/CarVer2.java
+++ src/pkg2/car/CarVer2.java
@@ -1,8 +1,8 @@
 package pkg2.car;
 /*	 < Car 클래스 >
- * ver1
+ * Ver1
 */
 public class CarVer2 {
-	public String name;  // public 접근 제어자 명시해 다른 패키지에서도 접근 가능.
+	public String name;  //
 	public int number;
 }
