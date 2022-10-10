f05759df33dda6f3345b275516fcd287f268e5bb
stadia <stadia1012@gmail.com>
2022-10-11 02:18:03 +0900

    생성자, this 끝

--- src/pkg1/javaintro/L19_Constructor.java
+++ src/pkg1/javaintro/L19_Constructor.java
@@ -1,4 +1,5 @@
 package pkg1.javaintro;
+import pkg2.car.*;
 
 public class L19_Constructor {
 //			   < 생성자 >
@@ -13,8 +14,17 @@ public class L19_Constructor {
 	 * 생성자를 명시해주지 않으면 매개변수 없는 생성자가 컴파일 시 자동으로 생성됨.
 	 * 
 	 */
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
-		 
+		Car car1 = new Car();  // pkg2의 Car는 생성자가 없으므로 매개변수 없는 생성자 자동 생성됨
+		
+//		CarVer2 car2 = new CarVer2();  // 에러. CarVer2는 생성자가 지정되어 있어서 해당하는 인자를 필히 입력해야
+		
+		CarVer2 car3 = new CarVer2( "소방차", 1234 );
+		
+		System.out.println( car3.name + ", "+ car3.number );  // 소방차, 1234 
 	}
 }
--- src/pkg2/car/CarVer2.java
+++ src/pkg2/car/CarVer2.java
@@ -1,8 +1,14 @@
 package pkg2.car;
-/*	 < Car 클래스 >
- * Ver1
+/*	 < Car ver2 클래스 >
+ * Ver2 : 생성자 추가
 */
 public class CarVer2 {
+	 
 	public String name;  //
 	public int number;
+	
+	public CarVer2 ( String name, int number) {
+		this.name = name;
+		this.number = number;
+	}
 }
