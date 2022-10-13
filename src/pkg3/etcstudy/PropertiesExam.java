8fcffe5765d7c61aabb48a64e2a91182e7c4c835
stadia1012 <stadia1012@gmail.com>
2022-10-13 11:02:11 +0900

    properties 추가

---
+++ src/pkg3/etcstudy/PropertiesExam.java
@@ -0,0 +1,22 @@
+package pkg3.etcstudy;
+import java.util.Properties;
+import java.io.FileNotFoundException;  // 예외 처리
+import java.io.IOException;  // 예외 처리
+import java.io.FileInputStream;
+
+public class PropertiesExam {
+
+	public static void main(String[] args) throws IOException {
+		
+		String resource = "src/pkg3/etcstudy/properties_exam.properties";
+		
+		Properties properties = new Properties();
+		
+		FileInputStream fis = new FileInputStream( resource );
+        
+        properties.load(new java.io.BufferedInputStream(fis));
+		
+		String str1 = properties.getProperty( "message" );
+		System.out.println( str1 );  // Hi
+	}
+}
