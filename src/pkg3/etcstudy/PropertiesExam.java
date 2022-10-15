3c7518f1afc626eeca5ca2c469d8ca2ad85ff047
stadia <stadia1012@gmail.com>
2022-10-15 23:24:04 +0900

    properties

--- src/pkg3/etcstudy/PropertiesExam.java
+++ src/pkg3/etcstudy/PropertiesExam.java
@@ -9,14 +9,13 @@ public class PropertiesExam {
 	public static void main(String[] args) throws IOException {
 		
 		String resource = "src/pkg3/etcstudy/properties_exam.properties";
-		
 		Properties properties = new Properties();
-		
 		FileInputStream fis = new FileInputStream( resource );
-        
         properties.load(new java.io.BufferedInputStream(fis));
 		
 		String str1 = properties.getProperty( "message" );
 		System.out.println( str1 );  // Hi
+		
+		System.out.println( properties.getProperty( "message2" ) );  // Hi2
 	}
 }
