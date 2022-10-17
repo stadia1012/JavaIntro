d243e86bd61f29aecaedea473a95a49d946e8f97
stadia <stadia1012@gmail.com>
2022-10-17 23:48:07 +0900

    복습

--- src/pkg3/etcstudy/PropertiesExam.java
+++ src/pkg3/etcstudy/PropertiesExam.java
@@ -14,7 +14,7 @@ public class PropertiesExam {
         properties.load(new java.io.BufferedInputStream(fis));
 		
 		String str1 = properties.getProperty( "message" );
-		System.out.println( str1 );  // Hi
+		System.out.println( str1 );  // Hi 
 		
 		System.out.println( properties.getProperty( "message2" ) );  // Hi2
 	}
