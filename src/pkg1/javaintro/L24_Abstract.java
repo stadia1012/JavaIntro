abb89e5ca9c7851ba4e3d01b7ec173d4dc6f3065
stadia <stadia1012@gmail.com>
2022-10-30 23:52:59 +0900

    추상클래스 복습

--- src/pkg1/javaintro/L24_Abstract.java
+++ src/pkg1/javaintro/L24_Abstract.java
@@ -2,7 +2,11 @@ package pkg1.javaintro;
 import pkg2.methods.*;
 
 public class L24_Abstract {
-//			< 추상클래스 >   -> pkg2의 Bird, Duck 클래스 사용
+//			< 추상클래스 >   -> pkg2의 Bird(추상클래스), Duck(구현한 클래스) 클래스 사용
+/* 	# 추상클래스 특징 :
+ *	상속받은 추상클래스의 추상메서드는 반드시 구현해야한다.
+ *	추상클래스는 인스턴스화 할 수 없다.
+ */
 	public static void main(String[] args) {
 		
 		Duck d1 = new Duck( "오리1" );  // 생성자를 통한 name 입력
